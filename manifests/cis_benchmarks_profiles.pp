a71059d52f4d569bc0371466ab40b3c1d156151b
Adam Buxton <buxton.adam@gmail.com>
2018-09-26 07:31:31 +0100

    ensure class is not invocable

---
+++ manifests/cis_benchmarks_profiles.pp
@@ -0,0 +1,9 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include cis_benchmark_profiles::cis_benchmarks_profiles
+class cis_benchmark_profiles::cis_benchmarks_profiles {
+  fail("${module_name} is a not supported class")
+}
